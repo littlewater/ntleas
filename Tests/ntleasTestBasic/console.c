f838ec7fd4eb0175dac246508ad371671132acdb
littlewater <littlewaterxp@126.com>
2025-03-10 21:20:11 +0800

    修改测试代码，阻止直接返回，辅助调试X64代码。

--- Tests/ntleasTestBasic/console.c
+++ Tests/ntleasTestBasic/console.c
@@ -5,5 +5,7 @@ int main(int argc, char const* argv[])
 {
 	fprintf(stderr, "A program just print one line.\n");
 
-	return (0);
+	// 添加暂停
+	printf("Press any key to continue...\n");
+	return getchar(); // 等待用户输入
 }
